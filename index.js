bdb1a561ded94bfd5277df46200031b95021feba
MARIATITUSRAJ <titusraj30@gmail.com>
2025-06-04 22:31:36 +0530

    message

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require ('express');
 const mongoose = require ('mongoose');
 const cors =  require ('cors');
-const Task = require ('/Task');
+const Task = require ('./models/Task');
 
 const app = express ();
 app.use (cors());
@@ -10,14 +10,14 @@ app.use (express.json());
 mongoose.connect('mongodb+srv://titusraj30:4eWSQGVgw5icPOTS@inventory-management.ih453.mongodb.net/?retryWrites=true&w=majority&appName=Inventory-Management',{
     useNewUrlParser: true,
     useUnifiedTopology: true,
-
-});
+}).then(()  =>  console.log('connected to database'))
+    .catch(err  =>  console.error ('Connection Error:',err));
 
 //Routes
 app.get('/tasks', async (req,res)  =>{
-    const task =new Task(req.body);
-    await task.save();
-    res.status(201).json(task);
+    const tasks = await Task.find();
+    res.json(tasks);
+    
 });
 
 app.post('/tasks', async(req,res) =>{
@@ -27,13 +27,13 @@ app.post('/tasks', async(req,res) =>{
 });
 app.put('/tasks/:id', async (req,res)  =>{
     const updated = await
-    TaskfindByIdAndUpdate(req.params.id, req.body, {new: true});
-    res.json(updaated);
+    Task.findByIdAndUpdate(req.params.id, req.body, {new: true});
+    res.json(updated);
 });
 
 app.delete('/tasks/:id',async (req,res)  => {
     await 
-    Task.findByIdAndDelete(req.params.is);
+    Task.findByIdAndDelete(req.params.id);
     res.status(204).end();
 
 });
